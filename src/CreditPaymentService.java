428cefe5b7129881387218bd10190efc267d8d92
lst47 <linh20121@gmail.com>
2022-07-19 21:52:46 +0300

    Delete out, .idea

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,11 +1,12 @@
 public class CreditPaymentService {
     public double calculate(int a, double b, int c) {
-        double summPay;
-        double f;
-        double percentM = b / 100 / c;
-        double math = Math.pow(1+percentM,c);
-        summPay = (percentM*math)/(math-1);
-        f = summPay * a;
-        return f;
+        //double summPay;
+        //double f;
+        double percentM = b / (100 * c);
+        //double math = Math.pow(1+percentM,c);
+        // summPay = (percentM*math)/(math-1);
+        //f = summPay * a;
+        double math = a * (percentM / (1 - (Math.pow((1 + percentM), -c))));
+        return math;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,17 @@ public class Main {
         int summ = 1000000;
         double percent = 9.99;
         int year = 12;
-        int doubleYear = 24;
-        int tripleYear = 36;
-        int q = (int) service.calculate(summ,percent,year);
-        int g = (int) service.calculate(summ,percent,doubleYear);
-        int f = (int) service.calculate(summ,percent,tripleYear);
+        int pay = (int) service.calculate(summ,percent,year);
+        System.out.println("При сроке кридита в 12 месяцев, платеж - "+pay);
 
-        System.out.println("При сроке кридита в 12 месяцев, платеж - "+q);
-        System.out.println("При сроке кридита в 24 месяцев, платеж - "+g);
-        System.out.println("При сроке кридита в 36 месяцев, платеж - "+f);
+        year = 24;
+        pay = (int) service.calculate(summ,percent,year);
+        System.out.println("При сроке кридита в 12 месяцев, платеж - "+pay);
+
+        year = 36;
+        pay = (int) service.calculate(summ,percent,year);
+        System.out.println("При сроке кридита в 12 месяцев, платеж - "+pay);
+
+
     }
 }
